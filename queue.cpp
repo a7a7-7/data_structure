cdfa6cc9f92c41328ff2e07095366e5f76536b90
a7a7-7 <hungmin090929@gmail.com>
2025-03-27 23:29:34 +0900

    Is this the end?

--- queue.cpp
+++ queue.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 class queue_yamae {
     private:
-        int queue[1357911];
+        int queue[10];
         int m_rear = -1, m_front = -1;
 
     public:
@@ -12,21 +12,44 @@ class queue_yamae {
             return m_rear == m_front;
         }
 
+        bool full() {
+            return m_rear == 10 - 1;
+        }
+
         void push(int num) {
+            if (full()) {
+                cout << "queue is full, cannot push anymore!" << endl;
+                return;
+            }
             m_rear++;
             queue[m_rear] = num;
         }
 
         int pop() {
-            if (empty())
-                return;
+            if (empty()) {
+                cout << "queue is empty, cannot pop!" << endl;
+                return -1;
+            }
             return queue[++m_front];
         }
 };
 
 int main() {
     queue_yamae q;
+
+    // Add data over max capacity 
     q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+    q.push(5);
+    q.push(6);
+    q.push(7);
+    q.push(8);
+    q.push(9);
+    q.push(10);
+    q.push(11);
+
     cout << q.pop() << endl;
 
     if (q.empty())
--- stack.cpp
+++ stack.cpp
@@ -4,20 +4,28 @@ using namespace std;
 
 class stack_yamae {
 private:
-    int stack[11235813];
+    int stack[10];
     int top_index = -1;
 public:
     void push(int num) {
+        if (full()) {
+            cout << "cannot push anymore, max capacity!!" << endl;
+            return;
+        }
         top_index++;
         stack[top_index] = num;
     }
 
-    void pop() {
-        top_index--;
+    int pop() {
+        if (empty()) {
+            cout << "cannot pop anymore, stack is empty!" << endl;
+            return -1;
+        }
+        return stack[--top_index];
     }
 
-    int top() {
-        return stack[top_index];
+    bool full() {
+        return top_index == 10 - 1;
     }
 
     bool empty() {
@@ -29,10 +37,21 @@ public:
 
 int main() {
     stack_yamae stack;
-    stack.push(1024);
+    // Add data over 10 times
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    stack.push(4);
+    stack.push(5);
+    stack.push(6);
+    stack.push(7);
+    stack.push(8);
+    stack.push(9);
+    stack.push(10);
+    stack.push(11);
 
-    cout << stack.top() << endl;
-    stack.pop();
+    cout << stack.pop() << endl;
+
     if (stack.empty())
         cout << "empty" << endl;
     else
